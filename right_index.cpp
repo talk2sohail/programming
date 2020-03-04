da61fa3543b77ab203abf5bfad4273ec954c7f62
Sohail <talk2sohail100@gmail.com>
2020-03-04 23:45:00 +0530

    third day

--- right_index.cpp
+++ right_index.cpp
@@ -7,9 +7,9 @@ int left_index(vector<int> arr, int low, int high, int x){
 
     int mid = (high + low) / 2;
     
-    if( arr[mid] == x && (arr[mid - 1] != x || mid == 0))
+    if( arr[mid] == x && (arr[mid + 1] != x || mid == n-1))
         return mid;
-    if( arr[mid] >= x) return left_index(arr, low, mid -1 , x);
+    if( arr[mid] > x) return left_index(arr, low, mid -1 , x);
     else return left_index(arr, mid + 1, high, x);
 }
 
@@ -21,7 +21,7 @@ int main(){
     for(auto &x: arr) cin>>x;
     int index = left_index(arr, 0, n-1, x);
     if(index == -1) cout<<"NOT FOUND";
-    else cout<<"FIRST OCCURRENCE FOUND AT INDEX: "<<index;
+    else cout<<"LAST OCCURRENCE FOUND AT INDEX: "<<index;
     cout<<endl;
 
     return 0;
