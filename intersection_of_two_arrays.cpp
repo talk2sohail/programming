1367e85bf7645da5b8f71794b92f0bc1dcb150fc
Sohail <talk2sohail100@gmail.com>
2020-03-15 03:18:00 +0530

    thirteenth day

--- intersection_of_two_arrays.cpp
+++ intersection_of_two_arrays.cpp
@@ -15,34 +15,34 @@ void sort_by_sec(pair<string, int> &a, pair<string, int> &b){
 
 void print_mat(vector< vector<int> > &a ){
     for(auto i=0;i< (int) a.size();i++){
-        for(auto j=0;j< (int) a[i].size(); j++){
+        for(auto j=0;j< (int)a[i].size(); j++){
             cout<<a[i][j]<<" ";
         }
         cout<<endl;
     }
 }
 
-
-// O(m+n) time && O(m+n) space
-unordered_set<int> union_of( vector<int> &a, vector<int> &b){
-	
-	unordered_set<int> output;
-	for(auto x: a)
-		output.insert(x);
-	for(auto x: b)
-		output.insert(x);
-	return output;
+// O(mxn) time && O(m+n)
+vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+		
+		vector<int> output;
+		for(auto x: nums2){
+			auto it = find(nums1.begin(), nums1.end(), x); 
+			if( it != nums1.end()){
+				nums1.erase(it);
+				output.push_back(x);
+			}
+		}
+		return output;
+        
 }
 
+
 int main(){
-	
-	vector<int> m,n;
-	m = { 1,2,1,3,4};
-	n = { 3,5,6,2,7};
-	unordered_set<int> res = union_of(m,n);
-	for(auto x: res)
-		cout<<x<<" ";
+	vector<int> m = {4,9,5};
+	vector<int> n = {9,4,9,8,4};
+	vector<int> res = intersect(m,n);
+	for(auto x:res) cout<<x<<" ";
 	cout<<endl;
-	
     return 0;
 }
