83e691de5f8c63ad987ff2e1a3556c575cae3ed5
Sohail <talk2sohail100@gmail.com>
2020-05-21 19:59:23 +0530

    seg fault

---
+++ memory.c
@@ -0,0 +1,20 @@
+#include<stdlib.h>
+
+#define ONE_K 1024 
+
+int main(){
+	char *some_mem;
+	char *scan_ptr;
+
+	some_mem = (char *)malloc(ONE_K);
+	if(some_mem == NULL) exit(EXIT_FAILURE);
+
+	scan_ptr = some_mem;
+	while(1){
+		*scan_ptr = '\0';
+		scan_ptr++;
+	}
+	exit(EXIT_FAILURE);
+
+	return 0;
+}
