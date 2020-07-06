f728e8610dfc5820b4c40b73aa0c67dcca90df8e
talk2sohail <talk2sohail100@gmail.com>
2020-07-07 00:52:22 +0530

    rename fact

--- plusMinus.js
+++ plusMinus.js
@@ -1,20 +1,3 @@
-// // this is the solution i have submittedprocess.stdin.resume();
-// process.stdin.setEncoding("utf-8");
-// var arr = "";
-// process.stdin.on("data", function (chunk) {
-//   arr += chunk;
-// });
-// process.stdin.on("end", function () {
-//   arr = arr.split("\n");
-//   var n = parseInt(arr.shift()); // in order ot get no of test cases
-//   for (let i = 0; i < n; i++) {
-//     //loop through no of test cases.
-//     var a = parseInt(arr[i].split(" ")[0]); //get first no
-//     var b = parseInt(arr[i].split(" ")[1]); // get second no
-//     console.log(a + b); //output the result
-//   }
-// });
-
 "use strict";
 
 process.stdin.resume();
