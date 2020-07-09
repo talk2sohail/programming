0b0578215475b18b925531201b44cdf97bddf49e
talk2sohail <talk2sohail100@gmail.com>
2020-07-09 16:08:53 +0530

    correccted sorted list[1;5D[1;5D[1;5D[1;5D[1;5D[1;5D[1;5D[1;5D[D[D[Dremove from sorted list

--- LinkList/remove_from_sorted_list.cpp
+++ LinkList/remove_from_sorted_list.cpp
@@ -1,24 +1,26 @@
 
 
-
 //removes duplicate from sorted list in O(n) time
-Node * removeDuplicate(Node *root){
+Node *removeDuplicate(Node *root)
+{
 
-    if( root == NULL || root->next == NULL) return root;
+    if (root == NULL || root->next == NULL)
+        return root;
     Node *curr = root;
-    Node *prev ;
-    while( curr->next != NULL && prev != NULL){
-        
-        if(curr->data == prev->data){
-            prev->next = curr->next;
-            curr = curr->next;
-        }else{
-            prev = curr;
+    while (curr->next != NULL)
+    {
+
+        if (curr->data == curr->next->data)
+        {
+            Node *next = curr->next->next;
+            free(curr->next);
+            curr->next = next;
+        }
+        else
+        {
             curr = curr->next;
         }
     }
-    
+
     return root;
-
 }
-
