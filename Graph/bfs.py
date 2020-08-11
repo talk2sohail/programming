8ba7c95cabf8707ef7cd81e04e49ae2ec99c6d98
talk2sohail <talk2sohail100@gmail.com>
2020-08-11 23:59:22 +0530

    graph bfs

---
+++ Graph/bfs.py
@@ -0,0 +1,29 @@
+graph = {
+  'A' : ['B','C'],
+  'B' : ['D', 'E', 'R', 'S'],
+  'C' : ['F'],
+  'D' : [],
+  'E' : ['F'],
+  'F' : [],
+  'R' : ['S'],
+  'S' : ['R']
+}
+
+visited = [] # List to keep track of visited nodes.
+queue = []     #Initialize a queue
+
+def bfs(visited, graph, node):
+  visited.append(node)
+  queue.append(node)
+
+  while queue:
+    s = queue.pop(0) 
+    print (s, end = " ") 
+
+    for neighbour in graph[s]: ## graph[s] is list of nodes/child
+      if neighbour not in visited:
+        visited.append(neighbour)
+        queue.append(neighbour)
+
+# Driver Code
+bfs(visited, graph, 'A')
